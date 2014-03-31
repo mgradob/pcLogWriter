ac029503a71f37a35fc0b67926050eff7ef83ce6
Miguel Grado <mgradob@gmail.com>
2014-03-31 16:53:22 -0700

    Corrected the uploading, now ads the time it uploads data.

--- comFunctions.py
+++ comFunctions.py
@@ -24,75 +24,75 @@ def getAvailablePorts():
 # Format received string -------------------------------------------
 def formatMoistureData(dataIn):
 
-    handshakeLog = Logs.HandshakeLog
-    moistureLog1 = Logs.MoistureLog
-    moistureLog2 = Logs.MoistureLog
-    weatherLog = Logs.WeatherLog
-    pumpLog = Logs.PumpLog
-    timeoutLog = Logs.TimeoutLog
+    handshakeLog = Logs.HandshakeLog()
+    moistureLog1 = Logs.MoistureLog()
+    moistureLog2 = Logs.MoistureLog()
+    weatherLog = Logs.WeatherLog()
+    pumpLog = Logs.PumpLog()
+    timeoutLog = Logs.TimeoutLog()
 
     try:
     # First Protocol, reading shit tons of data, from tons of
     # sensors and stuff.
 
         # Get SOH
-        handshakeLog.soh = str(dataIn[15])
+        handshakeLog.soh = str(int(dataIn[15]))
         # Get the EOT
-        handshakeLog.eot = str(dataIn[51])
+        handshakeLog.eot = str(int(dataIn[51]))
 
         # Get Node1
-        moistureLog1.node_id = str(dataIn[16])
+        moistureLog1.node_id = str(int(dataIn[16]))
         # Set height1
         moistureLog1.height1 = 1
         # Get moistures1
-        moistureLog1.moisture1 = str(dataIn[17]) + '.' + str(dataIn[18])
+        moistureLog1.moisture1 = str(int(dataIn[17])) + '.' + str(int(dataIn[18]))
         # Set height2
         moistureLog1.height2 = 2
         # Get moistures2
-        moistureLog1.moisture2 = str(dataIn[19]) + '.' + str(dataIn[20])
+        moistureLog1.moisture2 = str(int(dataIn[19])) + '.' + str(int(dataIn[20]))
         # Set height3
         moistureLog1.height3 = 3
         # Get moistures3
-        moistureLog1.moisture3 = str(dataIn[21]) + '.' + str(dataIn[22])
+        moistureLog1.moisture3 = str(int(dataIn[21])) + '.' + str(int(dataIn[22]))
 
         # Get Node2
-        moistureLog2.node_id = str(dataIn[23])
+        moistureLog2.node_id = str(int(dataIn[23]))
         # Set height1
         moistureLog2.height1 = 1
         # Get moistures1
-        moistureLog2.moisture1 = str(dataIn[24]) + '.' + str(dataIn[25])
+        moistureLog2.moisture1 = str(int(dataIn[24])) + '.' + str(int(dataIn[25]))
         # Set height2
         moistureLog2.height = 2
         # Get moistures2
-        moistureLog2.moisture2 = str(dataIn[26]) + '.' + str(dataIn[27])
+        moistureLog2.moisture2 = str(int(dataIn[26])) + '.' + str(int(dataIn[27]))
         # Set height3
         moistureLog2.height3 = 3
         # Get moistures3
-        moistureLog2.moisture3 = str(dataIn[28]) + '.' + str(dataIn[29])
+        moistureLog2.moisture3 = str(int(dataIn[28])) + '.' + str(int(dataIn[29]))
 
         # Get Climate Node
-        weatherLog.node_id = str(dataIn[30])
+        weatherLog.node_id = str(int(dataIn[30]))
         # Get radiation
-        weatherLog.radiation = str((dataIn[31] << 8) | dataIn[32])
+        weatherLog.radiation = str(int((dataIn[31] << 8) | dataIn[32]))
         # Get atmospheric humidity
-        weatherLog.atmospheric_humidity = str(dataIn[33]) + '.' + str(dataIn[34])
+        weatherLog.atmospheric_humidity = str(int(dataIn[33])) + '.' + str(int(dataIn[34]))
         # Get atmospheric temperature
-        weatherLog.atmospheric_temperature = str(dataIn[35]) + '.' + str(dataIn[36])
+        weatherLog.atmospheric_temperature = str(int(dataIn[35])) + '.' + str(int(dataIn[36]))
         # Get wind speed
-        weatherLog.wind_speed = str(dataIn[37]) + '.' + str(dataIn[38])
+        weatherLog.wind_speed = str(int(dataIn[37])) + '.' + str(int(dataIn[38]))
 
         # Get Pump
-        pumpLog.node_id = str(dataIn[39])
-        pumpLog.relay_status = str(dataIn[40])
-        pumpLog.pulse_count = str((dataIn[41] << 8) | dataIn[42])
+        pumpLog.node_id = str(int(dataIn[39]))
+        pumpLog.relay_status = str(int(dataIn[40]))
+        pumpLog.pulse_count = str(int((dataIn[41] << 8) | dataIn[42]))
 
         # Get Timeouts
-        timeoutLog.consolidate = str(dataIn[44]) + '.' + str(dataIn[45])
-        timeoutLog.timeout_DAAD = str(dataIn[46])
-        timeoutLog.timeout_DA55 = str(dataIn[47])
-        timeoutLog.timeout_c = str(dataIn[48])
-        timeoutLog.timeout_climate_node = str(dataIn[49])
-        timeoutLog.timeout_pump_node = str(dataIn[50])
+        timeoutLog.consolidate = str(int(dataIn[44])) + '.' + str(int(dataIn[45]))
+        timeoutLog.timeout_DAAD = str(int(dataIn[46]))
+        timeoutLog.timeout_DA55 = str(int(dataIn[47]))
+        timeoutLog.timeout_c = str(int(dataIn[48]))
+        timeoutLog.timeout_climate_node = str(int(dataIn[49]))
+        timeoutLog.timeout_pump_node = str(int(dataIn[50]))
 
     # Second protocol, reading only 6 bytes, 3 pairs of humidity
     #     # Get moisture higher byte, "xx.__"
